49d60367af3340e37251ad25bd14328b3b6567b8
Erwin G <erwingaitano@gmail.com>
2022-10-29 15:06:29 -0500

    fixing base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import packageJson from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {},
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  const base = isDev ? '/' : `${packageJson.homepage}/`;
+
+  return {
+    base,
+    plugins: [react()],
+    test: {},
+  };
 });
