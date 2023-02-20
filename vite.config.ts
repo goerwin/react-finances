c158ea8a481e29a09fe129a84f4763c2aa32b31d
Erwin G <erwingaitano@gmail.com>
2023-02-20 03:37:08 -0500

    fixing image urls in manifest

--- vite.config.ts
+++ vite.config.ts
@@ -19,17 +19,17 @@ export default defineConfig(({ mode }) => {
           theme_color: '#ffffff',
           icons: [
             {
-              src: '/android-chrome-192x192.png',
+              src: `${base}android-chrome-192x192.png`,
               sizes: '192x192',
               type: 'image/png',
             },
             {
-              src: '/android-chrome-512x512.png',
+              src: `${base}android-chrome-512x512.png`,
               sizes: '512x512',
               type: 'image/png',
             },
             {
-              src: '/android-chrome-512x512.png',
+              src: `${base}android-chrome-512x512.png`,
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any maskable',
