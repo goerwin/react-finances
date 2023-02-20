0ceabf20fa0c1b81f527d0e2613232aa482e3499
Erwin G <erwingaitano@gmail.com>
2023-02-20 03:26:40 -0500

    added pwa features

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import packageJson from './package.json';
 
 // https://vitejs.dev/config/
@@ -11,7 +12,32 @@ export default defineConfig(({ mode }) => {
 
   return {
     base,
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        manifest: {
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: '/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: '/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
     // NOTE: hmr disabled makes tailwind to not work properly
     // server: {
     // hmr: false,
