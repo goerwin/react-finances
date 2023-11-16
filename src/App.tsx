66fd948cdd629dcd896b435ca685bdfad74792bd
Erwin Gaitán Ospino <goerwin@Erwins-MacBook-Pro.local>
2023-11-16 17:59:56 -0500

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -319,7 +319,8 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="flex flex-col direct-first-child:mt-auto overflow-auto fixed inset-0">
-        <div className="overflow-auto pt-24 px-4 py-4 flex-grow">
+        <h2 className="text-2xl mt-0 mb-2 font-bold text-center pt-8 px-4">Recientes</h2>
+        <div className="overflow-auto px-4 py-4 flex-grow">
           {lsDb?.db.actions.slice(0, 5).map((it) => (
             <ItemView
               key={it.id}
