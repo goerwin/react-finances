795f6265247b2dec121ee378e52342b8d4632f02
Erwin G <erwingaitano@gmail.com>
2025-02-01 06:35:32 -0500

    feat: changing icon

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 import packageJson from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isDev = mode === 'development';
-  const base = isDev ? '/' : `/react-finances/`;
-
+export default defineConfig(() => {
   return {
-    base,
+    base: '/react-finances/',
     define: {
       APP_VERSION: JSON.stringify(packageJson.version),
     },
@@ -23,20 +20,26 @@ export default defineConfig(({ mode }) => {
           background_color: '#242424',
           icons: [
             {
-              src: `${base}android-chrome-192x192.png`,
+              src: '/icon-chicken/web/icon-192.png',
               sizes: '192x192',
               type: 'image/png',
             },
             {
-              src: `${base}android-chrome-512x512.png`,
+              src: '/icon-chicken/web/icon-512.png',
               sizes: '512x512',
               type: 'image/png',
             },
             {
-              src: `${base}android-chrome-512x512.png`,
+              src: '/icon-chicken/web/icon-512.png',
               sizes: '512x512',
               type: 'image/png',
-              purpose: 'any maskable',
+              purpose: 'any',
+            },
+            {
+              src: '/icon-chicken/web/icon-512-maskable.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
             },
           ],
         },
